e528c2ddaa370a0d1239c32cad812eeedb77bff7
Carlos Erique Gaspar <eriquegasparcarlos@gmail.com>
2026-05-13 16:43:46 -0500

    fix: accept nullable password in checkPassword to avoid TypeError

--- src/Auth/AuthHelper.php
+++ src/Auth/AuthHelper.php
@@ -6,8 +6,11 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthHelper
 {
-    public static function checkPassword(string $password): bool
+    public static function checkPassword(?string $password): bool
     {
+        if (!$password) {
+            return false;
+        }
         $user = auth()->user();
         return $user && Hash::check($password, $user->password);
     }
